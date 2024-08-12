4f3a76b6ad6537c4ea83bd9c07c5cd1a36ec80a3
The_phantom walker <Neocoinkitt@gmail.com>
2024-08-12 16:23:47 +0000

    Added a new task to the task list function

--- run.py
+++ run.py
@@ -4,3 +4,8 @@ class ToDoList:
     def __init__(self):
         """Initializes an empty list to store tasks."""
         self.tasks = []
+
+
+    def add_task(self, task):
+        """Adds a new task to the tasks list."""
+        self.tasks.append({"task": task, "completed": False})
